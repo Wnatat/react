85bc6d24a69907f7acc17a8cffeef2273f8ec762
Walid Natat <walid.natat@me.com>
2019-01-07 13:39:05 +0000

    use minified css and include favicon to webpack

--- app/index.js
+++ app/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/components/App';
 import Styles from './scss/app.sass'
+import './images/favicon.ico'
 
 ReactDOM.render(<App />, document.getElementById('app'));
